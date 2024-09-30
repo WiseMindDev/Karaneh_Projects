e7133d35b75df637b9369a8117c7a30b802226f3
WiseMindDev <153547583+WiseMindDev@users.noreply.github.com>
2024-10-01 00:32:34 +0330

    .

--- app.js
+++
@@ -1,9 +0,0 @@
-function showSidebar() {
-  const Sidebar = document.querySelector(".mobile__sidebar");
-  Sidebar.style.display = "flex";
-}
-
-function hideSidebar() {
-  const Sidebar = document.querySelector(".mobile__sidebar");
-  Sidebar.style.display = "none";
-}
